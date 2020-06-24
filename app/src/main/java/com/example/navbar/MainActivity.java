f52ffb14f88f0ef330b454a0e4492e3760b02d67
Rohitohlyan66 <ohlyanrohit66@gmail.com>
2020-06-24 17:26:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navbar/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.navbar;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    DrawerLayout drawer;
+    ActionBarDrawerToggle toggle;
+    NavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.toolbar);
+        navigationView=findViewById(R.id.nav_bar);
+        drawer=findViewById(R.id.layout);
+
+        setSupportActionBar(toolbar);
+
+        toggle=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.open,R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
+
+
+    }
+}
